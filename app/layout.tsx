8ba2ef7965ad4868f4541b25f2b3244234c3e112
Abdiqani paldoz <Abdiqanidahirabdullahi@gmail.com>
2026-05-24 18:54:39 +0300

    Beautiful PWA icon - dad figure with KG scale, blue gradient

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="DadCare" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <link rel="apple-touch-icon" href="/icons/icon.svg" />
       </head>
       <body className={`${inter.className} h-full antialiased`}>
         <ThemeProvider
